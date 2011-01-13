9ec84ba39063e9dbcdc0f52b00a19ae13b270ebe
therg <ryan@therg.net>
2011-01-12 16:06:26 -0800

    updated postgres 8.4.4 formula

--- Library/Formula/postgresql84.rb
+++ Library/Formula/postgresql84.rb
@@ -1,7 +1,7 @@
 require 'formula'
 require 'hardware'
 
-class Postgresql <Formula
+class Postgresql84 <Formula
   homepage 'http://www.postgresql.org/'
   url 'http://ftp2.uk.postgresql.org/sites/ftp.postgresql.org/source/v8.4.4/postgresql-8.4.4.tar.bz2'
   md5 '4bf2448ad965bca3940df648c02194df'
@@ -9,7 +9,7 @@ class Postgresql <Formula
   depends_on 'readline'
   depends_on 'libxml2' if MACOS_VERSION < 10.6 #system libxml is too old
 
-  aka 'postgres'
+  #aka 'postgres84' #this is deprecated 
 
   def options
     [
