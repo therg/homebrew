ee7ebf39f550480a990c1f3e45db562f364ad7c4
atkinson <joey.atkinson@SignOnSanDiego.com>
2011-10-13 14:56:32 -0700

    updated to 5.3.8

--- Library/Formula/php.rb
+++ Library/Formula/php.rb
@@ -5,10 +5,10 @@ def mysql_installed?
 end
 
 class Php <Formula
-  url 'http://www.php.net/get/php-5.3.5.tar.gz/from/this/mirror'
+  url 'http://www.php.net/get/php-5.3.8.tar.gz/from/this/mirror'
   homepage 'http://php.net/'
-  md5 'fb727a3ac72bf0ce37e1a20468a7bb81'
-  version '5.3.5'
+  md5 'f4ce40d5d156ca66a996dbb8a0e7666a'
+  version '5.3.8'
 
   # So PHP extensions don't report missing symbols
   skip_clean ['bin', 'sbin']
